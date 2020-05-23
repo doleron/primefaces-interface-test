4d885d21b687c2cd7dca5b65be8d8e01433ac031
doleron <doleron@gmail.com>
2020-05-23 17:53:30 -0300

    firefox automation via geckodriver

--- automation-test/README.md
+++ automation-test/README.md
@@ -10,14 +10,13 @@ To execute the test battery:
 - then, execute: 
 
 ```
-mvn test -Dselenium.base.url=http://localhost:8080/primefaces-test-webapp/ -Dwebdriver.chrome.driver=drivers\chromedriver.exe -Dselenium.run.headless=true -Dselenium.quit.browser.after.run=true
+mvn clean test -Dselenium.base.url=http://localhost:8080/primefaces-test-webapp/ -Dwebdriver.chrome.driver=drivers\chromedriver.exe -Dwebdriver.gecko.driver=D:\drivers\geckodriver.exe -Dselenium.run.headless=true -Dselenium.quit.browser.after.run=true
 
 ```
-This command will run all the test cases. You can limit the test cases using `-Dtest` parameter
-For example:
+This command will run the full battery of test cases. You can limit the test cases by using `-Dtest` parameter. For example the command:
 
 ```
-mvn test -Dselenium.base.url=http://localhost:8080/primefaces-test-webapp/ -Dwebdriver.chrome.driver=drivers\chromedriver.exe -Dselenium.run.headless=true -Dselenium.quit.browser.after.run=true -Dtest=org.primefaces.component.fileupload.cases.messagetemplate.advanced.auto.*Test
+mvn clean test -Dselenium.base.url=http://localhost:8080/primefaces-test-webapp/ -Dwebdriver.chrome.driver=D:\drivers\chromedriver.exe -Dwebdriver.gecko.driver=D:\drivers\geckodriver.exe -Dselenium.run.headless=true -Dselenium.quit.browser.after.run=true -Dtest=org.primefaces.component.fileupload.cases.messagetemplate.advanced.auto.*Test
 ```
 limits the execution for tests in package `org.primefaces.component.fileupload.cases.messagetemplate.advanced.auto.*` only.
 
@@ -25,7 +24,11 @@ Please check the Configuration section below for more details.
 
 ## Configuration
 
-- Make sure you haev the [chromedriver](https://chromedriver.chromium.org/) executable in somewhere of your computer and set `-Dwebdriver.chrome.driver` according.
+- Make sure you have the web drivers
+ - [chromedriver](https://chromedriver.chromium.org/) 
+ - 
+ 
+ executables in somewhere of your computer and set the parameters `-Dwebdriver.chrome.driver` and  `webdriver.gecko.driver` properly.
 
 - `-Dselenium.run.headless` can be set to `false` to show the browser screen during each test run.
 - If `selenium.quit.browser.after.run` is `false` the browser screen will keep visible after the test finishes. 
--- automation-test/src/test/java/org/primefaces/component/fileupload/FileUploadTest.java
+++ automation-test/src/test/java/org/primefaces/component/fileupload/FileUploadTest.java
@@ -1,7 +1,7 @@
 package org.primefaces.component.fileupload;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -11,71 +11,111 @@ import java.time.Duration;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.FluentWait;
 
 public abstract class FileUploadTest {
+	
+	private static ChromeDriver chromeDriver;
+	private static FirefoxDriver firefoxDriver;
     
     protected WebDriver driver;
-    protected URL baseUrl;
-    private boolean quitBrowserAfterRun;
+    protected static URL baseUrl;
+    private static boolean quitBrowserAfterRun;
+    private static boolean headless;
 
-    @Before
-    public void setUp() throws Exception {
-        
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("window-size=1024x768");
-        options.addArguments("--no-sandbox");
-
-        String headless = System.getProperty("selenium.run.headless");
-        if (headless == null || headless.equalsIgnoreCase("true")) {
-            options.addArguments("headless");
-        }
+    @BeforeClass
+    public static void setUp() throws Exception {
         
         String baseUrlParam = System.getProperty("selenium.base.url");
         if (baseUrlParam == null || baseUrlParam.trim().isEmpty()) {
             throw new RuntimeException("selenium.base.url not found in System.getProperty");
         }
-        this.baseUrl = new URL(baseUrlParam.trim());
+        baseUrl = new URL(baseUrlParam.trim());
 
         String quitBrowserAfterRunParam = System.getProperty("selenium.quit.browser.after.run");
-        this.quitBrowserAfterRun = quitBrowserAfterRunParam == null
+        quitBrowserAfterRun = quitBrowserAfterRunParam == null
                 || quitBrowserAfterRunParam.equalsIgnoreCase("true");
 
-        this.driver = new ChromeDriver(options);
-        this.driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        String headlessParam = System.getProperty("selenium.run.headless");
+        headless = headlessParam == null || headlessParam.equalsIgnoreCase("true");
+
+        initializeFirefoxDriver();
+        initializeChromeDriver();
+    }
+
+   private static void initializeFirefoxDriver() {
+    	FirefoxOptions options = new FirefoxOptions();
+    	options.addArguments("window-size=1024x768");
+        if(headless) {
+        	options.addArguments("--headless");
+        }
+    	firefoxDriver = new FirefoxDriver(options);
+        commonsDriverSettings(firefoxDriver);
+    }
+
+	private static void initializeChromeDriver() throws MalformedURLException {
+		ChromeOptions options = new ChromeOptions();
+        options.addArguments("window-size=1024x768");
+        options.addArguments("--no-sandbox");
+        if(headless) {
+        	options.addArguments("headless");
+        }
+        
+        chromeDriver = new ChromeDriver(options);
+        commonsDriverSettings(chromeDriver);
+	}
+    
+    private static void commonsDriverSettings(WebDriver webDriver) {
+    	webDriver.manage().deleteAllCookies();
+    	webDriver.manage().timeouts().pageLoadTimeout(2, TimeUnit.SECONDS);
+    	webDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
     }
     
     protected void init(String viewId) throws MalformedURLException {
-        URL pageAddress = new URL(this.baseUrl, viewId);
+        URL pageAddress = new URL(baseUrl, viewId);
         this.driver.get(pageAddress.toString());
     }
 
     @Test
-    public void test() throws Exception {
+    public void testChrome() throws Exception {
+        this.driver = chromeDriver;
+        this.execute();
+    }
+
+    @Test
+    public void testFirefox() throws Exception {
+    	this.driver = firefoxDriver;
         this.execute();
     }
 
     public abstract void execute() throws Exception;
 
-    @After
-    public void tearDown() throws Exception {
-        try {
-            if (this.quitBrowserAfterRun) {
-                this.driver.quit();
-            }
-        } catch (Error e) {
-            e.printStackTrace();
-        }
-    }
+	@AfterClass
+	public static void tearDown() throws Exception {
+		if (quitBrowserAfterRun) {
+			try {
+				firefoxDriver.quit();
+			} catch (Error e) {
+				e.printStackTrace();
+			}
+			try {
+				firefoxDriver.quit();
+			} catch (Error e) {
+				e.printStackTrace();
+			}
+		}
+	}
     
     protected void input(String clientId, String ... relativeFilePaths) {
         WebElement inputElement = this.driver.findElement(By.id(clientId));
@@ -85,10 +125,11 @@ public abstract class FileUploadTest {
             Path path = Paths.get(relativeFilePath);
             absolutePaths.append(path.toAbsolutePath().toString());
             if(i < relativeFilePaths.length - 1) {
-                absolutePaths.append(" \n ");
+                absolutePaths.append("\n");
             }
         }
-        inputElement.sendKeys(absolutePaths.toString());
+        String absolutePath = absolutePaths.toString();
+		inputElement.sendKeys(absolutePath);
     }
     
     protected void clickButton(By by) {
